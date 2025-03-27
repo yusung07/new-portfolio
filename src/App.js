2e6c495ce72f7bdc37a0bd04efc0fdc5cd4d6492
Dayoon07 <gangd0642@gmail.com>
2025-03-28 07:11:43 +0900

    DNS 필요할 줄은 몰랐어

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Contact from './pages/Contact';
 
 export default function App() {
   return (
-    <BrowserRouter basename='https://dayoon07.me'>
+    <BrowserRouter basename='/react-popol'>
       <div className="flex min-h-screen">
         <SideBar />
         <MobileSideBar />
